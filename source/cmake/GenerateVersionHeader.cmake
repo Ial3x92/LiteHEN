b00126ead7a870681753e575a150e62a27fed83e
Ial3x <alex92_14@live.it>
2026-08-28 00:32:30 +0200

    Update GenerateVersionHeader.cmake

--- source/cmake/GenerateVersionHeader.cmake
+++ source/cmake/GenerateVersionHeader.cmake
@@ -58,8 +58,9 @@ string(REGEX REPLACE "[^A-Za-z0-9._+-]" "-" _version "${_version}")
 string(CONCAT _contents
     "#pragma once\n\n"
     "/* Generated at build time. Do not edit. */\n"
-    "#define ONIONHEN_VERSION \"LiteHEN\"\n"
-    "#define ONIONHEN_AUTHOR \"Ial3x92\"\n")
+    "#define LITEHEN_VERSION \"LiteHEN\"\n"
+    "#define LITEHEN_AUTHOR \"Ial3x92\"\n")
+
 
 get_filename_component(_output_dir "${OUTPUT_FILE}" DIRECTORY)
 file(MAKE_DIRECTORY "${_output_dir}")
