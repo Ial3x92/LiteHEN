7afeb4d06e437e40c23056a426f451cdd58d625b
Ial3x92 <alex92_14@live.it>
2026-09-02 07:30:08 +0200

    Add files via upload

--- source/cmake/GenerateVersionHeader.cmake
+++ source/cmake/GenerateVersionHeader.cmake
@@ -58,7 +58,7 @@ string(REGEX REPLACE "[^A-Za-z0-9._+-]" "-" _version "${_version}")
 string(CONCAT _contents
     "#pragma once\n\n"
     "/* Generated at build time. Do not edit. */\n"
-    "#define ONIONHEN_VERSION \"OnionLiteHEN\"\n"
+    "#define ONIONHEN_VERSION \"LiteHEN\"\n"
     "#define ONIONHEN_AUTHOR \"Ial3x92\"\n")
 
 get_filename_component(_output_dir "${OUTPUT_FILE}" DIRECTORY)
