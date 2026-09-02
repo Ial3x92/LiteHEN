373ede09f77b19012e45ea03d1ba91ce02b6fe9a
Ial3x92 <alex92_14@live.it>
2026-09-02 08:27:20 +0200

    Delete source directory

--- source/libonion_platform/include/onion/platform.h
+++
@@ -1,9 +0,0 @@
-/* Copyright (C) 2025 OnionHEN / LightningMods
- *
- * Umbrella header for platform leaf helpers.
- */
-#pragma once
-
-#include <onion/fs.h>
-#include <onion/log.h>
-#include <onion/notify.h>
