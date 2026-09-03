abe33b67c58886ad46854ed6f5a3d52c354b0b98
Ial3x92 <alex92_14@live.it>
2026-09-03 07:59:51 +0200

    Add files via upload

--- source/bootstrapper/source/bootstrap_notify.c
+++ source/bootstrapper/source/bootstrap_notify.c
@@ -21,6 +21,6 @@ void bootstrap_notify_starting(bool custom_icon_ready) {
     return;
   }
   onion_notify_rich("notify.brand", "notify.boot.starting",
-                    "/user/data/LiteHEN/litehen.png", "download",
+                    "/user/data/OnionHEN/onionhen.png", "download",
                     "588193128");
 }
